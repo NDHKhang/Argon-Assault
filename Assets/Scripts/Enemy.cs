4d0e7427237e52d2cd583db24e39bf1938599c03
Khang Nguyen <dhoang.khang1504@gmail.com>
2024-08-29 09:50:35 +0700

    Update Code and Scene

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,3 +1,4 @@
+using NUnit.Framework;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,38 +7,54 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject DeathFX;
     [SerializeField] GameObject HitVFX;
+
     [SerializeField] int score = 10;
     [SerializeField] int hitPoint = 4;
 
     ScoreBoard scoreBoard;
-    GameObject parentGameObject;
+    GameObject parentGameObject; // Storing gameobject spawn at runtime
+
+    public List<ParticleCollisionEvent> collisionEvents;
+
 
     void Start()
     {
         scoreBoard = FindAnyObjectByType<ScoreBoard>();
         parentGameObject = GameObject.FindWithTag("SpawnAtRuntime");
-        AddRigridBody();
+        AddRigridBody(); // Adding Rigridbody at runtime instead of manual
+        collisionEvents = new List<ParticleCollisionEvent>();
     }
 
     void AddRigridBody()
     {
-        Rigidbody rb = gameObject.AddComponent<Rigidbody>();
-        rb.useGravity = false;
+        if(gameObject.GetComponent<Rigidbody>() == null)
+        {
+            Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+            rb.useGravity = false;
+            rb.isKinematic = true; // Avoid enemy collide with each other or the terrain
+        }   
     }
 
     void OnParticleCollision(GameObject other)
     {
-        ProcessHit();
+        ParticleSystem part = other.GetComponent<ParticleSystem>();
+        int numCollisionEvents = part.GetCollisionEvents(this.gameObject, collisionEvents);
+        
+        foreach (ParticleCollisionEvent collisionEvent in collisionEvents)
+        {
+            Vector3 pos = collisionEvent.intersection;
+            ProcessHit(pos);
+        }
         if (hitPoint == 0)
         {
             KillEnemy();
         }
     }
 
-    void ProcessHit()
+    void ProcessHit(Vector3 intersectionPos)
     {
         hitPoint--;
-        GameObject vfx = Instantiate(HitVFX, transform.position, Quaternion.identity);
+        GameObject vfx = Instantiate(HitVFX, intersectionPos, Quaternion.identity);
         vfx.transform.parent = parentGameObject.transform;
     }
 
